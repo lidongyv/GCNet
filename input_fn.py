6693f3f3f5b6860ea9dc52646cda07978e5dd015
pobingwanghai <pobingwanghai@gmail.com>
2017-06-21 19:53:59 +0800

    test for png

--- input_fn.py
+++ input_fn.py
@@ -36,8 +36,24 @@ Return:
     olfilenames=gfile.Glob(os.path.join(FLAGS.data_dir,'*','left','*.png'))
     orflilenames=gfile.Glob(os.path.join(FLAGS.data_data,'*','right','*.png'))
     glfilenames=gfile.Glob(os.path.join(FLAGS.data_data,'*','left','*.pfm'))
-    glfilenames=gfile.Glob(os.path.join(FLAGS.data_data,'*','right','*.pfm'))
+    grfilenames=gfile.Glob(os.path.join(FLAGS.data_data,'*','right','*.pfm'))
     if not lfilenames or not not lfilenames or not gfilenames:
         raise RuntimeError('No data files found.')
     index=len(lfilenames)
+    olimages=tf.train.string_input_producer(olfilenames,shuffle=False)
+    orimages=tf.train.string_input_producer(orflilenames,shuffle=False)
+    gldisparity=tf.train.string_input_producer(glfilenames,shuffle=False)
+    grdisparity=tf.tranin.string_input_producer(grfilenames,shuffle=False)
+    reader=tf.WholeFileReader()
+    key,limagev=reader.read(olimages)
+    limage=tf.image.decode_png(limagev)
+    key,rimagev=reader.read(orimages)
+    rimage=tf.image.decode_png(rimagev)
+
+
+
+
+
+
+
     
---
+++ test_pfm.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 21 11:15:18 2017
+
+@author: lidong
+"""
+
+import os
+
+import numpy as np
+import tensorflow as tf
+from python_pfm import *
+from tensorflow.python.platform import flags
+from tensorflow.python.platform import gfile
+import cv2
+gfilenames=gfile.Glob(os.path.join(r'D:\stereo dataset\Stereo Matching\disparity\family_x2\left','*.pfm'))
+a=readPFM(gfilenames[1])
+b=np.floor(a[0])
+c=cv2.imread(r'D:\stereo dataset\Stereo Matching\frames_cleanpass\35mm_focallength\scene_backwards\fast\left\0001.png')
+win = cv2.namedWindow('test win', flags=0)
+cv2.imshow('test win', b.astype(np.uint8))
+cv2.waitKey(0)  
